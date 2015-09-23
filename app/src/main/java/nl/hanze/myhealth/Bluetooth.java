0dcd519ef2178eb3114fb6796d0c9c8f8e74d1ae
Jeroen Hoekstra <jeroenhoekstra_1991@hotmail.com>
2015-09-23 18:25:34 +0200

    Removes Scanner import

--- app/src/main/java/nl/hanze/myhealth/Bluetooth.java
+++ app/src/main/java/nl/hanze/myhealth/Bluetooth.java
@@ -15,7 +15,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.util.Scanner;
 import java.util.Set;
 import java.util.UUID;
 
