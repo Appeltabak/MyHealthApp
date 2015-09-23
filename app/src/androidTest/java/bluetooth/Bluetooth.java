89bf38ca0cba81869fedee9112b2ee0c14f53770
Jeroen Hoekstra <jeroenhoekstra_1991@hotmail.com>
2015-09-23 14:17:03 +0200

    Adds bluetooth class stub

---
+++ app/src/androidTest/java/bluetooth/Bluetooth.java
@@ -0,0 +1,49 @@
+package bluetooth;
+
+/**
+ * Created by Jeroen on 23-9-2015.
+ */
+public class Bluetooth {
+    /**
+     * Scan for available bluetooth devices.
+     */
+    public void scan() {
+
+    }
+
+    /**
+     * Start listening for an incomming bluetooth connection.
+     */
+    public void listen() {
+
+    }
+
+    /**
+     * Connect to the bluetooth device.
+     */
+    public void connect() {
+
+    }
+
+    /**
+     * Terminate the bluetooth connection.
+     */
+    public void disconnect() {
+
+    }
+
+    /**
+     * Send a message to the connected bluetooth device.
+     * @param msg
+     */
+    public void send(String msg) {
+
+    }
+
+    /**
+     * Reads one line of data received from the connected bluetooth device.
+     */
+    public String readLine() {
+        return null;
+    }
+}
