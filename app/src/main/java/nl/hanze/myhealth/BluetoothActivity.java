0e8c15daf9d329a30eeb28c8e6c1c322d562eee8
Jeroen Hoekstra <jeroenhoekstra_1991@hotmail.com>
2015-09-28 10:19:16 +0200

    Removes unused imports

--- app/src/main/java/nl/hanze/myhealth/BluetoothActivity.java
+++ app/src/main/java/nl/hanze/myhealth/BluetoothActivity.java
@@ -8,9 +8,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.ListView;
 import android.widget.Toast;
 
 import java.io.IOException;
